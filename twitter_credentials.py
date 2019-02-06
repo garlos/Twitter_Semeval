4e18955f7e5354f613d9192ab2f3a3472715d207
VHMK <wahid.hosseinzadeh@gmail.com>
2019-02-06 17:23:35 +0330

    Initial commit

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+# Variables that contains the user credentials to access Twitter API
+ACCESS_TOKEN = "937694302720942080-h2MEtczNfa8ZOy68fzabZj7CzLpyGDG"
+ACCESS_TOKEN_SECRET = "lkG4Ro3eESKEsNH34TFU0zsEiFfOGpbHJSgiHAmq09CE1"
+CONSUMER_KEY = "qDZjZd4ADq0bL3QZQWLBbrXHM"
+CONSUMER_SECRET = "iYGq4Ig3FUjKj7vk0mkii6TSSOJ9ue1M28obUanfkYn0QxUguv"
